fae73132b8f18d2dd8ae327230824ec2c401c790
daniel-nguyen <daniel.n.q.nguyen@gmail.com>
2021-10-14 15:49:18 +1100

    Create butterfly

--- 07-databases/butterflies/main.rb
+++ 07-databases/butterflies/main.rb
@@ -9,18 +9,32 @@ end
 
 # INDEX: Show all butterflies
 get '/butterflies' do
-  # create a DB connection
-  db = SQLite3::Database.new 'database.sqlite3'
-  db.results_as_hash = true
-
-  # execute a query
-  @butterflies = db.execute 'SELECT * FROM butterflies'
-
-  # close the DB connection
-  db.close
-
-  # pass the data down to the view
+  @butterflies = query_db 'SELECT * FROM butterflies'
   erb :butterflies_index
 end
 
-# binding.pry
+# NEW: Shows a form for new butterfly data
+get '/butterflies/new' do
+  erb :butterflies_new
+end
+
+# CREATE: Inserts a new butterfly into the database
+post '/butterflies' do
+  query_db "INSERT INTO butterflies (name, family, image) VALUES ('#{ params[:name] }', '#{ params[:family] }', '#{ params[:image] }')"
+  redirect to('/butterflies')
+end
+
+# SHOW: Show all the information about a SINGLE butterfly
+get '/butterflies/:id' do
+  @butterfly = (query_db "SELECT * FROM butterflies WHERE id=#{params[:id]}").first
+  erb :butterflies_show
+end
+
+def query_db sql_statement
+  puts sql_statement # Optional, but very nice for debugging
+  db = SQLite3::Database.new 'database.sqlite3'
+  db.results_as_hash = true
+  results = db.execute sql_statement
+  db.close
+  results
+end
