35d079d6751bbed61108457cdb2d15aa772ec459
daniel-nguyen <daniel.n.q.nguyen@gmail.com>
2021-10-20 12:49:30 +1100

    NEW, CREATE pages

--- 08-rails/moma/app/controllers/artists_controller.rb
+++ 08-rails/moma/app/controllers/artists_controller.rb
@@ -4,12 +4,20 @@ class ArtistsController < ApplicationController
   end
 
   def new
+    @artist = Artist.new # a blank Artist template that Rails can use to create the form
   end
 
   def create
+    artist = Artist.create artist_params
+    redirect_to artist
   end
 
   def show
     @artist = Artist.find params[:id]
+  end
+
+  private # this makes ALL the following methods private (so only use it at the end)
+  def artist_params # strong params (see docs)
+    params.require(:artist).permit(:name, :nationality, :dob, :period, :image)
   end
 end
