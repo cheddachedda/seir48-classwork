419a35aa84f606a7dbddb983809ec84efcb10dff
daniel-nguyen <daniel.n.q.nguyen@gmail.com>
2021-10-21 13:44:44 +1100

    User has_secure_password and :through associations

--- 08-rails/tunr/db/schema.rb
+++ 08-rails/tunr/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_21_000939) do
+ActiveRecord::Schema.define(version: 2021_10_21_014125) do
 
   create_table "albums", force: :cascade do |t|
     t.text "title"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 2021_10_21_000939) do
     t.text "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
