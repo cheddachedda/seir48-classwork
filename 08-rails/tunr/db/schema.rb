7b5c0af9720b8cad53537a7527bfe09d7ce95622
daniel-nguyen <daniel.n.q.nguyen@gmail.com>
2021-10-22 10:55:26 +1100

    Authenticated access, admin access

--- 08-rails/tunr/db/schema.rb
+++ 08-rails/tunr/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_21_014125) do
+ActiveRecord::Schema.define(version: 2021_10_21_233254) do
 
   create_table "albums", force: :cascade do |t|
     t.text "title"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 2021_10_21_014125) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
