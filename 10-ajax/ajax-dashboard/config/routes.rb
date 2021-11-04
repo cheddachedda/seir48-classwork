c2f6f9e4ec79a573cd2f46d4fdc72146c6ab4e03
daniel-nguyen <daniel.n.q.nguyen@gmail.com>
2021-11-04 13:43:50 +1100

    Add routes

--- 10-ajax/ajax-dashboard/config/routes.rb
+++ 10-ajax/ajax-dashboard/config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root :to => 'pages#home'
+
+  # Endpoints
+  get '/api/brother' => 'dashboard#brother'
+  get '/api/time' => 'dashboard#time'
+  get '/api/uptime' => 'dashboard#uptime'
+  get '/api/calls' => 'dashboard#calls'
+  get '/api/bushfires' => 'dashboard#bushfires'
+
+  get '/api/info' => 'dashboard#info'
 end
