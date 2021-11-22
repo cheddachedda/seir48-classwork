d179e9635bc51fd589815bac919200aef43b2ee2
daniel-nguyen <daniel.n.q.nguyen@gmail.com>
2021-11-22 12:25:27 +1100

    Refactor match multiple words any order

--- 14-advanced/regular-expressions/ex6.rb
+++ 14-advanced/regular-expressions/ex6.rb
@@ -1,7 +1,5 @@
 # 6. [8] Extra credit exercise: write a program that prints out any input line that mentions both wilma and fred.
 
 ARGF.each do |line|
-  wilma_fred = /\b[Ww]ilma\b.*\b[Ff]red\b/
-  fred_wilma= /\b[Ff]red\b.*\b[Ww]ilma\b/
-  puts line if line =~ wilma_fred || line =~ fred_wilma
+  puts line if line =~ /(?=.*[Ww]ilma)(?=.*[Ff]red)/
 end
