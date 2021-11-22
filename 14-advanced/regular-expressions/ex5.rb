e4952d1022a13992c13257d2b75d1c77a5436347
daniel-nguyen <daniel.n.q.nguyen@gmail.com>
2021-11-22 12:54:34 +1100

    Match any doubled character

--- 14-advanced/regular-expressions/ex5.rb
+++ 14-advanced/regular-expressions/ex5.rb
@@ -1,5 +1,5 @@
 # 5. [8] Make a program that prints each line that has a two of the same nonwhitespace characters next to each other. It should match lines that contain words such as Mississippi, Bamm-Bamm, or llama.
 
 ARGF.each do |line|
-  puts line if line =~ /([A-Za-z])\1/
+  puts line if line =~ /(.)\1/
 end
